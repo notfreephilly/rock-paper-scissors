e1d64fe4b6360f676f8d199349813ec137185102
notfreephilly <not.philyoder93@gmail.com>
2024-04-09 23:30:44 -0700

    Established the computer to pick an option at random, function plays one round

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 // Function to randomly generate a choice selected by the computer
 
 
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"]; // Let computer pick option
+    const choices = ["Rock", "Paper", "Scissor"]; // Let computer pick option
     const randomChoice = Math.floor(Math.random() * choices.length + 1)
 
     if (randomChoice === 1) {
@@ -12,13 +15,43 @@ function getComputerChoice() {
         return "Paper"
     }
     else if (randomChoice === 3) {
-        return "Scissors"
+        return "Scissor"
     }
 }
 
-// 
 
-function playSingleRound(playerSelection, computerSelection) {
-    
+
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection;
+    let comp = computerSelection;
+
+    const playerStr = player.toLowerCase();
+    const compStr = comp.toLowerCase();
+
+    if (playerStr === "rock")
+        if (compStr === "rock") {
+            return "It is a tie!!"
+        } else if (compStr === "paper") {
+            return "You lose, paper beats rock!"
+        } else {
+            return "You won!"
+        }
+    else if (playerStr === "scissor")
+        if (compStr === "scissor") {
+            return "You tied!"
+        } else if (compStr === "rock") {
+            return "You lose, rock beats scissor!"
+        } else {
+            return "You won!"
+        }
+    else {
+        if (compStr === "paper") {
+            return "You lost! Paper beats rock"
+        } else if (compStr === "scissor") {
+            return "You win! Rock beats scissor"
+        } else {
+            return "Tie game!"
+        }
+    }
 }
 
